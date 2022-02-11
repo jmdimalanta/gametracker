a4ba852e37918c377d26515bebc86b7acd1bcdf6
Juan Miguel <jmdimalanta.jmd@gmail.com>
2022-02-10 19:25:27 -0800

    Added login/reg backend

---
+++ server/server.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+//Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(cors({
+    origin: "http://localhost:3000"
+}))
+
+//require config
+require("./config/mongoose.config")
+//require routes
+require("./routes/user.routes")(app);
+
+app.listen(process.env.MY_PORT, ()=> console.log(`You are connected to port ${process.env.MY_PORT}`))
