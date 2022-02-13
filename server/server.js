9c283b5fe356d36d2870b1a83feb177d785271ce
Juan Miguel <jmdimalanta.jmd@gmail.com>
2022-02-12 19:19:24 -0800

    updated authentication

--- server/server.js
+++ server/server.js
@@ -9,7 +9,9 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.use(cors({
+    credentials: true,
     origin: "http://localhost:3000"
+
 }))
 
 app.use(cookieParser());
